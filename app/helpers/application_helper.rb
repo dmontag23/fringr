86a17197de033f05c4899a2a9f0ef76b74d75fb7
dmontag23 <dmontag23@gmail.com>
2016-10-18 11:54:37 -0600

    Change title of pages and corresponding tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,12 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title = '')
-		page_title + " | " + "Fringr"
+    base_title = "Fringr"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
   
 end
