4eac8e5d1879dfcb8cf8f3c63a90f401373a1d9d
dmontag23 <dmontag23@gmail.com>
2016-10-18 20:19:07 -0600

    Add signup page functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+  
+  # Get request to /users/new (/signup)
   def new
+  	@user = User.new
   end
+
+  # Post request to /users
+  def create
+    @user = User.new(secure_params)
+    if @user.save
+      flash[:success] = "Welcome to Fringr #{@user.name}!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  	# Ensures the use of strong parameters
+    def secure_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
