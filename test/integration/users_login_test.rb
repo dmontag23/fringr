12b004b0fcc33b5bb795395f998f4626cd00c17e
dmontag23 <dmontag23@gmail.com>
2016-10-20 12:33:35 -0600

    Add different home page for logged in users with corresponding integration test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -25,6 +25,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'static_pages/home'
     assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 0
+    assert_select "a[href=?]", login_path,  count: 0
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -32,6 +34,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'static_pages/home'
     assert_select "a[href=?]", login_path,       count: 2
+    assert_select "a[href=?]", signup_path,      count: 1
     assert_select "a[href=?]", logout_path,      count: 0
   end
 
