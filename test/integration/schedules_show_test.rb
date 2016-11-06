a03c2f0caa4a5cdf8d167c956d13176e1c11ab29
dmontag23 <dmontag23@gmail.com>
2016-11-06 12:51:27 -0700

    Add integration tests for pieces controller

--- test/integration/schedules_show_test.rb
+++ test/integration/schedules_show_test.rb
@@ -5,6 +5,8 @@ class SchedulesShowTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @schedule = @user.schedules.find_by(name: "Fringe 2016")
+    @piece = pieces(:manburns)
+    @piece.participants.create!(contact_id: 4)
     log_in_as @user
   end
 
@@ -22,6 +24,84 @@ class SchedulesShowTest < ActionDispatch::IntegrationTest
       assert_select "a[href=?]", schedule_piece_path(@schedule, piece)
       assert_select "a[href=?]", edit_schedule_piece_path(@schedule, piece)
       assert_select 'a', text: "Delete"
+    end
+  end
+
+  test "new piece display" do
+    get new_schedule_piece_path(@schedule)
+    assert_template 'pieces/new'
+    assert_select 'title', full_title("New Piece")
+  end
+
+  test "edit piece display" do
+    get edit_schedule_piece_path(@schedule, @piece)
+    assert_template 'pieces/edit'
+    assert_select 'title', full_title("Edit Piece")
+  end
+
+  test "show piece display" do
+    get schedule_piece_path(@schedule, @piece)
+    assert_template 'pieces/show'
+    assert_select 'title', full_title("#{@piece.title}")
+  end
+
+  test "unsucessful addition of a piece" do 
+    assert_no_difference 'Piece.count' do
+      assert_no_difference 'Participant.count' do
+        post schedule_pieces_path(@schedule), params: { piece: { title: "    ", length: 30, setup: 15 , cleanup: 5, 
+                                                                     location_id: 1, rating: 3, contact_ids: ["1", "3"] } }
+        assert_template 'pieces/new'
+        assert_select 'div[class=?]', 'alert alert-danger'
+      end
+    end
+  end
+
+  test "sucessful creation of a piece" do 
+    assert_difference 'Piece.count', +1 do
+      assert_difference 'Participant.count', +2 do
+        post schedule_pieces_path(@schedule), params: { piece: { title: "Test", length: 30, setup: 15 , cleanup: 5, 
+                                                                     location_id: 1, rating: 3, contact_ids: ["1", "3"] } }
+        assert_redirected_to @schedule
+        follow_redirect!
+        assert !flash.empty?
+        assert_select 'div[class=?]', 'alert alert-success'
+      end
+    end
+  end
+
+  test "unsucessful edit of a piece" do 
+    assert_no_difference 'Piece.count' do
+      assert_no_difference 'Participant.count' do
+        patch schedule_piece_path(@schedule, @piece), params: { piece: { title: "Test", length: -30, setup: 15 , cleanup: 5, 
+                                                                     location_id: 1, rating: 3 } }
+        assert_template 'pieces/edit'
+        assert_select 'div[class=?]', 'alert alert-danger'
+      end
+    end
+  end
+
+  test "sucessful edit of a piece" do 
+    assert_no_difference 'Piece.count' do
+      assert_difference 'Participant.count', +2 do
+        patch schedule_piece_path(@schedule, @piece), params: { piece: { title: "Test", length: 30, setup: 15 , cleanup: 5, 
+                                                                     location_id: 1, rating: 3, contact_ids: ["1", "3", "2"] } }
+        assert_redirected_to @schedule
+        follow_redirect!
+        assert !flash.empty?
+        assert_select 'div[class=?]', 'alert alert-success'
+      end
+    end
+  end
+
+  test "sucessful deletion of a schedule" do
+    assert_difference 'Piece.count', -1 do
+      assert_difference 'Participant.count', -1 do
+        delete schedule_piece_path(@schedule, @piece)
+        assert_redirected_to @schedule
+        follow_redirect!
+        assert !flash.empty?
+        assert_select 'div[class=?]', 'alert alert-success'
+      end
     end
   end
   
